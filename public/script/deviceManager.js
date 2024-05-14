8ba508523b233746ac7347128c618aacae3a686e
KoliaSafuanov <66707781+KoliaSafuanov@users.noreply.github.com>
2024-05-14 21:15:12 +0500

    Update deviceManager.js

--- public/script/deviceManager.js
+++ public/script/deviceManager.js
@@ -7,8 +7,19 @@ function getDevices() {
     $.get("/getDevices")
     .done(function (result) {
         for (var i = 0; i < result.length; i++) {
-           //
+            $("table tbody").append("<tr><th scope='row'>" + (i + 1) + "</th><td><img src='css/icons/" + result[i].device + ".png'" + " class='thumbnail' alt='...'></td><td><a href='javascript:void(0);' onclick='manageDevice(" + result[i].deviceId + ")'><img class='thumbnail' src='css/icons/stop.png' id='device" + result[i].deviceId + "'/> </a></td></tr>");
+            setStateIcon(result[i]);
         }
     });
 }
 
+function manageDevice(deviceId) {
+    $.post("/", { "deviceId": deviceId })
+    .done(function (result) {
+        setStateIcon(result);
+    });
+}
+
+function setStateIcon(result) {
+        $("#device" + result.deviceId).attr('src', result.status ? 'css/icons/stop.png' : 'css/icons/start.png');
+}
